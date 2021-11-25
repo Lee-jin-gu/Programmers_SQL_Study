a3fa0335b8331d0f0a67d8843b7f9d9f5ec87747
DRJingu <30xlofa9ligtgs@naver.com>
2021-11-25 13:51:11 +0900

    Level 2

---
+++ Level 2/Level 2_고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,3 @@
+SELECT Animal_id, name, sex_upon_intake from animal_ins
+where Name in ('Mitty', 'Lucy','Sabrina','Rogan','Ella','Pickle')
+order by animal_id
---
+++ Level 2/Level 2_동물 수 구하기.sql
@@ -0,0 +1,1 @@
+SELECT count(datetime) from animal_ins
